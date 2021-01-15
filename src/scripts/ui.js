571cd57644892b3f0065031fff64e7083a61a581
Kevin Earley <kearley89@gmail.com>
2021-01-15 15:36:53 -0500

    sass and js refactor, add link on album img

--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -4,21 +4,49 @@ class UI {
     this.artistInput02 = document.querySelector('.artist-input-02');
     this.submitBtn = document.querySelector('.submit');
     this.results = document.querySelector('.results');
-    this.resultsAlbumTitle = document.querySelector('.results-album-title');
-    this.resultsAlbumArtist = document.querySelector('.results-album-artist');
-    this.resultsAlbumImg = document.querySelector('.results-album-img');
   }
   
   displayResults(similarArtistTopAlbum) {
-    this.resultsAlbumTitle.textContent = similarArtistTopAlbum.name;
-    this.resultsAlbumArtist.innerHTML = `<span class='by'>by</span> ${similarArtistTopAlbum.artist.name}`;
-    this.resultsAlbumImg.src = similarArtistTopAlbum.image[3]["#text"];
+    // create album title el and add to ui
+    const resultsAlbumTitle = document.createElement("h2");
+
+    resultsAlbumTitle.classList.add("results-album-title");
+
+    resultsAlbumTitle.textContent = similarArtistTopAlbum.name;
+
+    this.results.appendChild(resultsAlbumTitle);
+
+
+    // create album artist el and add to ui
+    const resultsAlbumArtist = document.createElement("h3");
+
+    resultsAlbumArtist.classList.add("results-album-artist");
+
+    resultsAlbumArtist.textContent = similarArtistTopAlbum.artist.name;
+
+    this.results.appendChild(resultsAlbumArtist);
+
+
+    // create album img el and add to ui
+    const resultsAlbumImgContainer = document.createElement("div");
+    const resultsAlbumLink = document.createElement("a");
+    const resultsAlbumImg = document.createElement("img");
+
+    resultsAlbumImgContainer.classList.add("results-album-img-container");
+    resultsAlbumLink.classList.add("results-album-link");
+    resultsAlbumImg.classList.add("results-album-img");
+
+    resultsAlbumLink.href = similarArtistTopAlbum.url;
+    resultsAlbumLink.setAttribute("target", "_blank");
+    resultsAlbumImg.src = similarArtistTopAlbum.image[3]["#text"];
+
+    resultsAlbumLink.appendChild(resultsAlbumImg);
+    resultsAlbumImgContainer.appendChild(resultsAlbumLink);
+    this.results.appendChild(resultsAlbumImgContainer);
   }
 
   clearResults() {
-    this.resultsAlbumTitle.textContent = '';
-    this.resultsAlbumArtist.textContent = '';
-    this.resultsAlbumImg.src = '#';
+    this.results.innerHTML = "";
   }
 
   displayLoader() {
